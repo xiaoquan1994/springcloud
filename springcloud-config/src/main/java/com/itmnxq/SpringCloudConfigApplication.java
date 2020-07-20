872b19e438adfe72939990509529eade804e20dc
huangqingquan <994190772@qq.com>
2020-07-20 16:27:47 +0800

    测试springcloud-config

--- springcloud-config/src/main/java/com/itmnxq/SpringCloudConfigApplication.java
+++ springcloud-config/src/main/java/com/itmnxq/SpringCloudConfigApplication.java
@@ -3,11 +3,13 @@ package com.itmnxq;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
-public class Application {
+public class SpringCloudConfigApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(SpringCloudConfigApplication.class, args);
     }
 }
