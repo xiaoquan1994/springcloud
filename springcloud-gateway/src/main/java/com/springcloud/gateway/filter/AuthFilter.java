4a92c0b16ce066879ff6d4aebbde592a5558504f
huangqingquan <994190772@qq.com>
2020-07-21 10:07:59 +0800

    测试springcloud-gateway

--- springcloud-gateway/src/main/java/com/springcloud/gateway/filter/AuthFilter.java
+++ springcloud-gateway/src/main/java/com/springcloud/gateway/filter/AuthFilter.java
@@ -2,6 +2,7 @@ package com.springcloud.gateway.filter;
 
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
@@ -10,7 +11,7 @@ import reactor.core.publisher.Mono;
 import java.util.List;
 
 @Component
-public class AuthFilter implements GlobalFilter {
+public class AuthFilter implements GlobalFilter, Ordered {
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
         ServerHttpRequest request = exchange.getRequest();
@@ -19,5 +20,10 @@ public class AuthFilter implements GlobalFilter {
             System.err.println("鉴权失败");
         }
         return chain.filter(exchange);
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
     }
 }
