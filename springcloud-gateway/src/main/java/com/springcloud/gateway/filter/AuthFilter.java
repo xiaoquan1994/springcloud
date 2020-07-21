b9e081f2762248a31d7ff910cb05dc5a480dda2e
huangqingquan <994190772@qq.com>
2020-07-21 10:33:58 +0800

    测试springcloud-gateway

--- springcloud-gateway/src/main/java/com/springcloud/gateway/filter/AuthFilter.java
+++ springcloud-gateway/src/main/java/com/springcloud/gateway/filter/AuthFilter.java
@@ -3,7 +3,9 @@ package com.springcloud.gateway.filter;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
+import org.springframework.core.io.buffer.DataBuffer;
 import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
@@ -15,9 +17,14 @@ public class AuthFilter implements GlobalFilter, Ordered {
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
         ServerHttpRequest request = exchange.getRequest();
+        ServerHttpResponse response = exchange.getResponse();
         List<String> authorization =request.getHeaders().get("Authorization");
         if(authorization==null||authorization.size()!=1){
             System.err.println("鉴权失败");
+            String resp = "{\"code\":\"9999\",\"msg\":\"鉴权失败！\"}";
+            DataBuffer bodyDataBuffer = response.bufferFactory().wrap(resp.getBytes());
+            response.getHeaders().add("Content-Type", "application/json;charset=UTF-8");
+            return response.writeWith(Mono.just(bodyDataBuffer));
         }
         return chain.filter(exchange);
     }
