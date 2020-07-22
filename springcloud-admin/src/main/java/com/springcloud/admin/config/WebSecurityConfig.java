ebd263286fb095660ff69a28bc046e1912c19942
huangqingquan <994190772@qq.com>
2020-07-22 10:46:05 +0800

    测试springcloud-admin

--- springcloud-admin/src/main/java/com/springcloud/admin/config/WebSecurityConfig.java
+++ springcloud-admin/src/main/java/com/springcloud/admin/config/WebSecurityConfig.java
@@ -1,16 +1,28 @@
 package com.springcloud.admin.config;
 
+import de.codecentric.boot.admin.server.config.AdminServerProperties;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 
+@EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-
         SavedRequestAwareAuthenticationSuccessHandler successHandler = new SavedRequestAwareAuthenticationSuccessHandler();
         successHandler.setTargetUrlParameter("redirectTo");
-        http.headers().frameOptions().disable();
-        http.csrf().disable().authorizeRequests().antMatchers("/actuator/**").permitAll().anyRequest().authenticated().and().httpBasic();
+
+        http.authorizeRequests()
+                .antMatchers("/assets/**").permitAll()
+                .antMatchers( "/login").permitAll()
+                .antMatchers( "/actuator/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().loginPage("/login").successHandler(successHandler)
+                .and()
+                .logout().logoutUrl("/logout").and()
+                .httpBasic().and()
+                .csrf().disable();
     }
 }
