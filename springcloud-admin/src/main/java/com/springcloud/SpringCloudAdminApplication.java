94875c2a727f91c43ba663127d1a72102d17228f
huangqingquan <994190772@qq.com>
2020-07-22 09:52:54 +0800

    测试springcloud-admin

--- springcloud-admin/src/main/java/com/springcloud/SpringCloudAdminApplication.java
+++ springcloud-admin/src/main/java/com/springcloud/SpringCloudAdminApplication.java
@@ -1,11 +1,13 @@
 package com.springcloud;
 
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableAdminServer
 @EnableDiscoveryClient
 @SpringBootApplication
 public class SpringCloudAdminApplication {
